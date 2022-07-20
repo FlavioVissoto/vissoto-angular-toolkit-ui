bd8fadda86b1fd20e742e82fa4d882a7014f47b6
Flavio Vieira Vissoto <vissoto_flavio@hotmail.com>
2022-07-20 16:30:48 -0300

    Exportação das interfaces do tooltip

--- projects/ui/src/public-api.ts
+++ projects/ui/src/public-api.ts
@@ -1,14 +1,23 @@
+/** Button */
 export * from './lib/button/button.component';
 export * from './lib/button/button.module';
+
+/** Card */
 export * from './lib/card/card.component';
 export * from './lib/card/card.module';
 export * from './lib/card/interface/card.interface';
+
+/** Select */
 export * from './lib/select/interfaces/iui-select.interface';
 export * from './lib/select/select.component';
 export * from './lib/select/select.module';
+
+/** Tab */
 export * from './lib/tab/interface/tab.interface';
 export * from './lib/tab/tab.component';
 export * from './lib/tab/tab.module';
 
+/** Tooltip */
+export * from './lib/tooltip/interface/position.enum';
 export * from './lib/tooltip/tooltip.component';
 export * from './lib/tooltip/tooltip.module';
