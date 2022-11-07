c842090c440a004bbc134d22b93a84460c7ce66b
Flavio Vieira Vissoto <vissoto_flavio@hotmail.com>
2022-11-07 15:52:47 -0300

    Componente InputText

--- projects/ui/src/lib/input-text/input-text.component.spec.ts
+++ projects/ui/src/lib/input-text/input-text.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { InputTextComponent } from './input-text.component';
+import { Size } from '../interface/size.enum';
 
 describe('InputTextComponent', () => {
   let component: InputTextComponent;
@@ -8,9 +9,8 @@ describe('InputTextComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ InputTextComponent ]
-    })
-    .compileComponents();
+      declarations: [InputTextComponent],
+    }).compileComponents();
   });
 
   beforeEach(() => {
@@ -19,7 +19,54 @@ describe('InputTextComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  test('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  test('parseInputEvent', () => {
+    const event = new Event('click');
+    const newEvent = component.parseInputEvent(event);
+    expect(newEvent).toBeTruthy();
+  });
+
+  test('parsePointerEvent', () => {
+    const event = new Event('click');
+    const newEvent = component.parsePointerEvent(event);
+    expect(newEvent).toBeTruthy();
+  });
+
+  test('getCss Rounded ExtraSmall', () => {
+    component.rounded = Size.ExtraSmall;
+    let css = component['getCssRounded']();
+    expect(css).toEqual('rounded-sm');
+  });
+
+  test('getCss Rounded Small', () => {
+    component.rounded = Size.Small;
+    let css = component['getCssRounded']();
+    expect(css).toEqual('rounded');
+  });
+
+  test('getCss Rounded Base', () => {
+    component.rounded = Size.Base;
+    let css = component['getCssRounded']();
+    expect(css).toEqual('rounded-md');
+  });
+
+  test('getCss Rounded Large', () => {
+    component.rounded = Size.Large;
+    let css = component['getCssRounded']();
+    expect(css).toEqual('rounded-lg');
+  });
+
+  test('getCss Rounded ExtraLarge', () => {
+    component.rounded = Size.ExtraLarge;
+    let css = component['getCssRounded']();
+    expect(css).toEqual('rounded-full');
+  });
+
+  test('getCss Rounded Null', () => {
+    let css = component['getCssRounded']();
+    expect(css).toEqual('');
   });
 });
