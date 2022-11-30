3ca4a267b3b51f1ce479ea879e3c959e847fae4d
Flavio Vieira Vissoto <vissoto_flavio@hotmail.com>
2022-11-30 17:16:08 -0300

    Componente Alert e testes de unidade

--- projects/ui/src/lib/alert/alert.component.spec.ts
+++ projects/ui/src/lib/alert/alert.component.spec.ts
@@ -8,9 +8,8 @@ describe('AlertComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ AlertComponent ]
-    })
-    .compileComponents();
+      declarations: [AlertComponent],
+    }).compileComponents();
   });
 
   beforeEach(() => {
@@ -19,7 +18,38 @@ describe('AlertComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  afterEach(() => {
+    jest.resetAllMocks();
+    jest.clearAllTimers();
+  });
+
+  test('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  test('set show equal true', () => {
+    component.show = true;
+    expect(component._show).toBeTruthy();
+  });
+
+  test('set show equal false', () => {
+    component.show = false;
+    expect(component._show).toBeFalsy();
+  });
+
+  test('close', () => {
+    component._show = true;
+    component.close();
+    expect(component._show).toBeFalsy();
+  });
+
+  test('timeoutClose', () => {
+    jest.useFakeTimers();
+    component.autoClose = true;
+    component.timer = 2000;
+    component._show = true;
+    component.timeoutClose();
+    jest.runAllTimers();
+    expect(component._show).toBeFalsy();
   });
 });
